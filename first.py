8390f51f68dd24664ab6677c5c2af0e60cd57e2d
marthaea <marthapraisekatusiime@gmail.com>
2025-09-09 10:38:07 +0300

    first oop

---
+++ first.py
@@ -0,0 +1,7 @@
+class StarterProgram:
+    def greet(self):
+        print("Hello, OOP World!")
+
+#creating an object (instance) of the class
+StarterProgram = StarterProgram()
+StarterProgram.greet()
